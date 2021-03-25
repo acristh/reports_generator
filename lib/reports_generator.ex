2b4e466a2a86e3d002b116381003d41dffe41192
acristh <andre.cristiano1975@gmail.com>
2021-03-25 15:36:41 -0300

    feat: create fetch_higher() and sum_values()

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -4,10 +4,14 @@ defmodule ReportsGenerator do
   def build(file_name) do
     file_name
     |>Parser.parse_file()
-    |>Enum.reduce(report_acc(), fn [id, food_name, price], report ->
-      Map.put(report, id, report[id] + price)
+    |>Enum.reduce(report_acc(), fn line, report ->
+      sum_values(line, report)
     end)
   end
+
+  def fetch_higher(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
+  defp sum_values([id, food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
